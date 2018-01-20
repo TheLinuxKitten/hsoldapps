ae39b5b41f2a9461c2b9d0f6aa2157dfbefec5a6
The Linux Kitten <thelinuxkitten@thelinuxkitten.org>
2018-01-20 23:45:56 +0100

    README

--- src/Ethereum/Solidity/Meta.hs
+++ src/Ethereum/Solidity/Meta.hs
@@ -17,6 +17,15 @@ import Language.Haskell.TH
 import Network.Web3.Dapp.EthABI.TH
 import Network.Web3.Dapp.EthABI.Types
 
+-- Quitar comentario y usar una dirección conocida, sino produce error
+-- de compilación
+{-
 $(downloadHttp "http://192.168.122.201:8543" defaultSwarmSettings
       $ HexEthAddr "0x39df13a96f46db1e62b673086c692736d74baa1f")
+-}
+
+-- Comentar código al quitar los comentarios anteriores.
+$(runIO getCurrentDirectory >>= \wd -> compile (SolcSettings [] [])
+    [ wd ++ "/src/Ethereum/Solidity/coin.sol"
+    ])
 
